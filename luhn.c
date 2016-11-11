d059c43fec9c2b31720062747c099b1a3dcdbc2d
bdlightner <lightner@lightner.net>
2016-11-11 14:27:22 -0800

    Create luhn.c

---
+++ luhn.c
@@ -0,0 +1,63 @@
+/* luhn.c - Luhn algorithm test program */
+
+#include <stdio.h>
+
+int check_number(char *number)
+{
+    int i, sum, ch, num, twoup;
+
+    sum = 0;
+    twoup = 0;
+    for (i = strlen(number) - 1; i >= 0; --i) {
+        ch = number[i];
+        num = (ch >= '0' && ch <= '9') ? ch - '0' : 0;
+        if (twoup) num = (num + num);
+        if (num > 9) num = (num / 10) + (num % 10);
+        sum += num;
+        twoup = ++twoup & 1;
+    }
+    sum = 10 - (sum % 10);
+    sum %= 10;
+    return (sum == 0) ? 1 : 0;
+}
+
+int calc_digit(char *number)
+{
+    int i, sum, ch, num, twoup;
+
+    sum = 0;
+    twoup = 1;
+    for (i = strlen(number) - 1; i >= 0; --i) {
+        ch = number[i];
+        num = (ch >= '0' && ch <= '9') ? ch - '0' : 0;
+        if (twoup) num = (num + num);
+        if (num > 9) num = (num / 10) + (num % 10);
+        sum += num;
+        twoup = ++twoup & 1;
+    }
+    sum = 10 - (sum % 10);
+    sum %= 10;
+    return sum;
+}
+
+main(int argc, char **argv)
+{
+    int ok;
+    int check_digit;
+    char *number;
+
+    if (argc <= 1) {
+        printf("usage: %s number ...\n", *argv);
+        exit(1);
+    }
+
+    while (--argc) {
+        ++argv;
+        number = *argv;
+        ok = check_number(number);
+        check_digit = calc_digit(number);
+        printf("%s %s check\n", number, (ok) ? "passes" : "fails");
+        printf("%s needs check-digit '%d'\n", number, check_digit);
+    }
+    exit(0);
+}
